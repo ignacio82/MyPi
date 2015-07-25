08eb60f982e4a6d3c0303057f5965882bab922b0
ignacio <rstudio@example.com>
2015-07-25 21:00:13 +0000

    adds names to the benchmark

--- R/Pibenchmark.R
+++ R/Pibenchmark.R
@@ -1,6 +1,6 @@
 Pibenchmark <- function(DARTS = 5000 ,ROUNDS = 100, times=10L){
-  PiRace <- microbenchmark::microbenchmark(Rpi(DARTS = DARTS ,ROUNDS = ROUNDS),
-                           Fpi(DARTS = DARTS, ROUNDS = ROUNDS),
+  PiRace <- microbenchmark::microbenchmark(R = Rpi(DARTS = DARTS ,ROUNDS = ROUNDS),
+                           Fortran = Fpi(DARTS = DARTS, ROUNDS = ROUNDS),
                            times = times)
   return(PiRace)
 
